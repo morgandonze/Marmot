f33e44e233eefb9011a044e1d7a264dfe07f9245
Morgan Donze <donzemorgan@gmail.com>
2025-03-20 12:57:59 -0400

    Write new options to json

--- index.js
+++ index.js
@@ -2,25 +2,41 @@ import { setTimeout } from 'node:timers/promises';
 import * as p from '@clack/prompts';
 import color from 'picocolors';
 
-async function main() {
-  p.intro(color.blue('Hello!'));
+import * as fs from 'fs';
 
-  await p.group(
-    {
-      list: () => p.select({
-        message: 'Choose an item',
-        initialValue: 'Apple',
-        maxItems: 5,
-        options: [
-          {value: 'Apple', label: 'Apple'},
-          {value: 'Banana', label: 'Banana'},
-          {value: 'Cucumber', label: 'Cucumber'},
-          {value: 'Dragonfruit', label: 'Dragonfruit'},
-          {value: 'Eggplant', label: 'Eggplant'},
-        ]
-      })
-    }
-  )
+function listHandler(options) {
+  return () => p.select({
+      message: 'Choose an item',
+      initialValue: options[0].value,
+      options: options
+    });
+}
+
+function addHandler() {
+  return () => p.text({
+        message: "Add a new item",
+        placeholder: "Enter an item..."
+  })
+}
+
+function deleteHandler() {
+}
+
+async function main() {
+  // Read options from json file
+  const data = fs.readFileSync('./data.json', 'utf-8');
+  const options = JSON.parse(data);
+
+  // Get input
+  console.clear();
+  const output = await addHandler(options)();
+
+  // Add new option to list
+  options.push({"value": output, label: output})
+
+  // Write to file
+  const updatedOptionsJson = JSON.stringify(options);
+  fs.writeFileSync('./newData.json', updatedOptionsJson);
 }
 
 main().catch(console.error)
